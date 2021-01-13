9851d8059cf254cd3a776b1103ac57b24b067412
Sam Bruns <sambruns2000@gmail.com>
2021-01-12 21:27:49 -0500

    Added a fruit that randomly populates that the snake can eat

--- Pygame/snake.py
+++ Pygame/snake.py
@@ -2,42 +2,51 @@ import sys, random
 import pygame as pg
 pg.init()
 
-size = width, height = 1000, 750
+size = width, height = 1000, 720
 speed = [0, 0]
 black = (0, 0, 0)
 white = (255, 255, 255)
+yellow = (241, 252, 25)
 blue = (0, 106, 255)
 clock = pg.time.Clock()
 
 screen = pg.display.set_mode(size)
 
-#ball = pg.image.load("intro_ball.gif")
-#snakeRect = ball.get_rect()
-
 def random_location():
-    x = random.randint(0, 39) * 25
-    y = random.randint(0, 29) * 25
+    x = random.randint(0, 49) * 20
+    y = random.randint(0, 35) * 20
     return (x, y)
 
+def fruit_center():
+    (x,y) = random_location()
+    x += 10
+    y += 10
+    center = (x, y)
+    return center
+
 # Rect(left, top, width, height) -> Rect
-snakeRect = pg.Rect(random_location(), (25, 25))
+snakeRect = pg.Rect(random_location(), (20, 20))
 # rect(screen, color, rect, width=0, border_radius=0)
 snake = pg.draw.rect(screen, blue, snakeRect, 0)
 
+center = fruit_center()
+# circle(surface, color, center, radius) -> Rect
+fruit = pg.draw.circle(screen, yellow, center, 10)
+
 while 1:
-    clock.tick(6)
+    clock.tick(10)
     for event in pg.event.get():
         if event.type == pg.QUIT: sys.exit()
         elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE: sys.exit()
         elif event.type == pg.KEYDOWN and event.key == pg.K_q: sys.exit()
         elif event.type == pg.KEYDOWN and event.key == pg.K_RIGHT:
-            speed = [25, 0]
+            speed = [20, 0]
         elif event.type == pg.KEYDOWN and event.key == pg.K_LEFT:
-            speed = [-25, 0]
+            speed = [-20, 0]
         elif event.type == pg.KEYDOWN and event.key == pg.K_DOWN:
-            speed = [0, 25]
+            speed = [0, 20]
         elif event.type == pg.KEYDOWN and event.key == pg.K_UP:
-            speed = [0, -25]
+            speed = [0, -20]
 
     snakeRect = snakeRect.move(speed)
 
@@ -46,6 +55,10 @@ while 1:
     if snakeRect.top < 0 or snakeRect.bottom > height:
         speed[1] = -speed[1]  # Change this to ending the game eventually
 
+    if pg.Rect.colliderect(snakeRect, fruit):
+        center = fruit_center()
+
     screen.fill(black)
+    fruit = pg.draw.circle(screen, yellow, center, 10)
     snake = pg.draw.rect(screen, blue, snakeRect, 0, 0)
     pg.display.flip()
